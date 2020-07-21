16e7159b3284e65c309dd68862f97ecf9e2bd5db
Kavitha <anikavi73@gmail.com>
2020-07-20 19:39:17 -0500

    Class 17 studio assignment

---
+++ studio/Rocket.js
@@ -0,0 +1,48 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var itemsSum = 0;
+        for (var i = 0; i < items.length; i++) {
+            itemsSum += items[i].massKg;
+        }
+        return itemsSum;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        var sumOfMass;
+        sumOfMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return sumOfMass;
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
---
+++ studio/Rocket.ts
@@ -0,0 +1,59 @@
+import { Payload } from "./Payload";
+import { Cargo } from "./Cargo";
+import { Astronaut } from "./Astronaut";
+
+export class Rocket implements Payload {
+    // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[]= [];
+    astronauts: Astronaut[]= [];
+    items: Payload[];
+    item: Payload;
+    cargo: Cargo;
+    astronaut: Astronaut;
+    massKg: number;
+    
+    constructor(name: string, totalCapacityKg: number){
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+        let itemsSum: number = 0;
+        for (let i:number = 0; i < items.length; i++){
+            itemsSum += items[i].massKg;
+        }
+        return itemsSum;
+    }
+
+    currentMassKg(): number {
+        let sumOfMass: number;
+        sumOfMass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return sumOfMass;
+    }
+
+    canAdd(item: Payload): boolean {
+        if(this.currentMassKg() + item.massKg <= this.totalCapacityKg){
+            return true;
+        }
+    }
+
+    addCargo(cargo:Cargo): boolean{
+        if(this.canAdd(cargo)){
+          this.cargoItems.push(cargo);
+          return true;
+        } else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if(this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
